8c3c671a1ddc09cddadac04b98b8d8adbfafd310
Davi Tostes <davisiqueira591@gmail.com>
2023-01-18 22:45:08 -0300

    eu sou burro

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -5,40 +5,46 @@ import { SiLinkedin } from "react-icons/si";
 import { SiTwitter } from "react-icons/si";
 
 import loadProfilePic from '../../utils/loadProfilePic';
-import { useEffect, useState } from 'react';
+import { Component, useEffect, useState } from 'react';
 
-const About = () => {
-  const [profilePic, setProfilePic] = useState('')
+class About extends Component {
+  state = {
+    profilePic: ''
+  }
 
-  useEffect(() => {
-    setProfilePic(loadProfilePic())
-  }, [])
+  async componentDidMount () {
+    this.setState({profilePic: await loadProfilePic()})
+  }
 
-  return (
-    <div className="about-wrapper" id='about'>
-      <div className="pic-wrapper">
-        <img src="src\assets\profilePic.jpg" alt="profile-pic" />
-      </div>
-      <div className="title-wrapper">
-        <h1>Hi, I'm Davi Tostes👋</h1>
-        <h1 id='dev'>Front-end Developer</h1>
-        <div className="line"></div>
-        <h2>About:</h2>
-        <p>
-          Desenvolvedor front-end com conhecimentos em React, JavaScript, HTML, CSS e Nextjs. Cursando bacharel em Sistemas de Informação e procurando
-          uma vaga para desenvolvedor junior ou estágio.
-        </p>
-        <div className="cv-wrapper">
-          <a href={profilePic} download>Download CV</a>
+  render() {
+    const {profilePic} = this.state
+
+    return (
+      <div className="about-wrapper" id='about'>
+        <div className="pic-wrapper">
+          <img src={profilePic} alt="profile-pic" />
         </div>
-        <div className="social-wrapper">
-          <a target="_blank" href="https://github.com/DaviTostes" ><FaGithub className='icon'/></a>
-          <a target="_blank" href="https://www.linkedin.com/in/davi-tostes-bb6642245/" ><SiLinkedin className='icon'/></a>
-          <a target="_blank" href="https://twitter.com/davi_tostes8" ><SiTwitter className='icon'/></a>
+        <div className="title-wrapper">
+          <h1>Hi, I'm Davi Tostes👋</h1>
+          <h1 id='dev'>Front-end Developer</h1>
+          <div className="line"></div>
+          <h2>About:</h2>
+          <p>
+            Desenvolvedor front-end com conhecimentos em React, JavaScript, HTML, CSS e Nextjs. Cursando bacharel em Sistemas de Informação e procurando
+            uma vaga para desenvolvedor junior ou estágio.
+          </p>
+          <div className="cv-wrapper">
+            <a href="src\assets\daviTostesCV.pdf" download>Download CV</a>
+          </div>
+          <div className="social-wrapper">
+            <a target="_blank" href="https://github.com/DaviTostes" ><FaGithub className='icon'/></a>
+            <a target="_blank" href="https://www.linkedin.com/in/davi-tostes-bb6642245/" ><SiLinkedin className='icon'/></a>
+            <a target="_blank" href="https://twitter.com/davi_tostes8" ><SiTwitter className='icon'/></a>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default About
